1e23a043f7eaa9d1006d9f8dd4af37b2ed6755ee
FAYEZA-ZOBAIR <fayeza.zobair@gmail.com>
2023-09-28 18:58:59 +0530

    add

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-// dc86f2bb9a884897904103916232509
-// http://api.weatherapi.com/v1/current.json?key=dc86f2bb9a884897904103916232509&q=Mumbai&aqi=no
+const apiKey="be10d29caf5c1267dc490611bc5faf4f";
+const apiUrl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
+const searchBox = document.querySelector(".search input"); 
+const searchBtn = document.querySelector(".search button"); 
+async function checkWeather(city){
+    const response =await fetch(apiUrl + city + `&appid=${apiKey}`);
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)  + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
+}
+searchBtn.addEventListener("click",()=>{
+    checkWeather(searchBox.value);
+})
